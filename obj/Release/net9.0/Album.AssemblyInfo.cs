1f32f934d5d3cbe874c5e0ae9ac971aa8f5a8233
frieder <fribahnie@t-online.de>
2025-07-08 09:58:11 +0200

    modified:   Baukasten/Werte/DefaultWerte.xml 	modified:   Fotoalben/2020_Sommer/Albumseiten/vierk/seite0000.html 	new file:   Fotoalben/2020_Sommer/Vorschaubilder/2019qq2019_07_21_35.Hochzeitstag_FischbachqqP1220482.JPG 	new file:   Fotoalben/2020_Sommer/Vorschaubilder/2019qq2019_07_21_35.Hochzeitstag_FischbachqqP1220498.JPG 	new file:   Fotoalben/2020_Sommer/Vorschaubilder/2019qq2019_07_21_35.Hochzeitstag_FischbachqqP1220499.JPG 	new file:   Fotoalben/2020_Sommer/Vorschaubilder/2019qq2019_07_21_35.Hochzeitstag_FischbachqqP1220500.JPG 	new file:   Fotoalben/2020_Sommer/Vorschaubilder/2019qq2019_07_21_35.Hochzeitstag_FischbachqqP1220502.JPG 	new file:   Fotoalben/2020_Sommer/Vorschaubilder/2019qq2019_07_21_35.Hochzeitstag_FischbachqqP1220503.JPG 	new file:   Fotoalben/2020_Sommer/Vorschaubilder/2019qq2019_07_21_35.Hochzeitstag_FischbachqqP1220504.JPG 	new file:   Fotoalben/2020_Sommer/Vorschaubilder/2019qq2019_07_21_35.Hochzeitstag_FischbachqqP1220505.JPG 	new file:   Fotoalben/2020_Sommer/Vorschaubilder/2019qq2019_07_21_35.Hochzeitstag_FischbachqqP1220506.JPG 	new file:   Fotoalben/2020_Sommer/Vorschaubilder/2019qq2019_07_21_35.Hochzeitstag_FischbachqqP1220509.JPG 	new file:   Fotoalben/2020_Sommer/Vorschaubilder/2019qq2019_07_21_35.Hochzeitstag_FischbachqqP1220511.JPG 	new file:   Fotoalben/2020_Sommer/Vorschaubilder/2019qq2019_07_21_35.Hochzeitstag_FischbachqqP1220512.JPG 	new file:   Fotoalben/2020_Sommer/Vorschaubilder/2019qq2019_07_21_35.Hochzeitstag_FischbachqqP1220513.JPG 	new file:   Fotoalben/2020_Sommer/Vorschaubilder/2019qq2019_07_21_35.Hochzeitstag_FischbachqqP1220514.JPG 	new file:   Fotoalben/2020_Sommer/Vorschaubilder/2019qq2019_07_21_35.Hochzeitstag_FischbachqqP1220516.JPG 	new file:   Fotoalben/2020_Sommer/Vorschaubilder/2019qq2019_07_21_35.Hochzeitstag_FischbachqqP1220519.JPG 	new file:   Fotoalben/2020_Sommer/Vorschaubilder/2019qq2019_07_21_35.Hochzeitstag_FischbachqqP1220520.JPG 	new file:   Fotoalben/2020_Sommer/Vorschaubilder/2019qq2019_07_21_35.Hochzeitstag_FischbachqqP1220521.JPG 	new file:   Fotoalben/2020_Sommer/Vorschaubilder/2020qq2020_07_24_Fischbach_BodenseeqqP1220806.JPG 	new file:   Fotoalben/2020_Sommer/Vorschaubilder/2020qq2020_07_24_Fischbach_BodenseeqqP1220807.JPG 	new file:   Fotoalben/2020_Sommer/Vorschaubilder/2020qq2020_07_24_Fischbach_BodenseeqqP1220808.JPG 	new file:   Fotoalben/2020_Sommer/Vorschaubilder/2020qq2020_07_24_Fischbach_BodenseeqqP1220817.JPG 	modified:   Fotoalben/albumname.txt 	modified:   bin/Release/net9.0/Album.dll 	modified:   bin/Release/net9.0/Album.pdb 	modified:   obj/Release/net9.0/Album.AssemblyInfo.cs 	modified:   obj/Release/net9.0/Album.AssemblyInfoInputs.cache 	modified:   obj/Release/net9.0/Album.dll 	modified:   obj/Release/net9.0/Album.pdb 	modified:   obj/Release/net9.0/Album.sourcelink.json 	modified:   obj/Release/net9.0/ref/Album.dll 	modified:   obj/Release/net9.0/refint/Album.dll

--- obj/Release/net9.0/Album.AssemblyInfo.cs
+++ obj/Release/net9.0/Album.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Album")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4157407cc680b8f0fb43f4bd25537ce0965cb3ec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c68b0c8ed13937332c29ab1a827b3103fe8d874")]
 [assembly: System.Reflection.AssemblyProductAttribute("Album")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Album")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

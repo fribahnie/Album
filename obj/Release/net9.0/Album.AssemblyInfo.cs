4157407cc680b8f0fb43f4bd25537ce0965cb3ec
frieder <fribahnie@t-online.de>
2025-07-08 09:36:23 +0200

    modified:   obj/Release/net9.0/Album.AssemblyInfo.cs modified:   obj/Release/net9.0/Album.AssemblyInfoInputs.cache

--- obj/Release/net9.0/Album.AssemblyInfo.cs
+++ obj/Release/net9.0/Album.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Album")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4ee182e60be0eecf22bfe3f55b4b18d24dc808e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Album")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Album")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

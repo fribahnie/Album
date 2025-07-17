63424b86411b7eaa68cf58bf9f6c88b2c94404b0
frieder <fribahnie@t-online.de>
2025-07-17 18:15:44 +0200

    umbenannt in save

--- obj/Debug/net9.0/Album.AssemblyInfo.cs
+++ obj/Debug/net9.0/Album.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Album")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6dfb0452697cbd40220842f9c715fef51f679f95")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bf7149c8ba52986a00660d01af4b561e3faaf50")]
 [assembly: System.Reflection.AssemblyProductAttribute("Album")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Album")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

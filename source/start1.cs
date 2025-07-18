67d1061a5d4bb0cd1de3571a3e68288d770a22d4
frieder <fribahnie@t-online.de>
2025-07-18 11:20:00 +0200

    using reduziert

--- source/start1.cs
+++ source/start1.cs
@@ -1,13 +1,9 @@
 using System;
 using System.IO;
-using System.Xml;
-using System.Collections;
-using System.Runtime.InteropServices;
 using System.Collections.Generic;
 using Gtk;
 using ModulePlattform;
 using ModuleSprachen;
-using Startfenster;
 
 namespace Startfenster
 {

4f03d4bbd425943ab5f5ead5294eba7d2f945ba7
frieder <fribahnie@t-online.de>
2025-07-19 11:09:29 +0200

    SchlankeBins

--- bin/Release/net9.0/linux-x64/publish/starteAlbum.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd ~/csharp/gtk3-apps/Album/bin/Release/net9.0/linux-x64/publish/
-./Album
-
--- obj/Release/net9.0/win-x64/Album.AssemblyInfo.cs
+++ obj/Release/net9.0/win-x64/Album.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Album")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac02140bfab613078d89db5d12a40a75d3f1d3ec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+358315be5646ac1b5340e400f38aa1c1fd0f444b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Album")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Album")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

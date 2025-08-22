494f9c1ba1641f49b8d6993d1aba221fe34ba515
frieder <fribahnie@t-online.de>
2025-08-22 10:52:21 +0200

    neu Trixie

--- obj/Debug/net9.0/Album.AssemblyInfo.cs
+++ obj/Debug/net9.0/Album.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Album")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c0befae4b296dc0534100abcc463f944d0f3561")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c20df514d4a95643ca8025917025ea298954d71")]
 [assembly: System.Reflection.AssemblyProductAttribute("Album")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Album")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

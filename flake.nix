48310e109d858200b7dda8d24caafdc06c6f07d6
frieder <fribahnie@t-online.de>
2026-09-05 17:02:22 +0200

    jetzt mit vscodium

--- flake.nix
+++ flake.nix
@@ -30,7 +30,7 @@
           projectFile = "Album.csproj";
           executables = [ "Album" ];
 
-          dotnet-sdk = pkgs.dotnet-sdk_9;
+          dotnet-sdk = pkgs.dotnet-sdk_10;
           nugetDeps = ./deps.json;
 
           nativeBuildInputs = with pkgs; [
@@ -50,65 +50,76 @@
             icu
           ];
 
-		  postInstall = ''
-		    # Icon installieren
-		    install -Dm644 \
-		      Baukasten/Beispielbilder/Logo/album.png \
-		      $out/share/icons/hicolor/256x256/apps/album.png
+	  postInstall = ''
+          # Icon installieren
+	  install -Dm644				      \
+	  Baukasten/Beispielbilder/Logo/album.png			\
+	  $out/share/icons/hicolor/256x256/apps/album.png
 		  
-		    # Ressourcen ins Store-Verzeichnis kopieren
-		    mkdir -p $out/share/Album
-		    cp -r Baukasten $out/share/Album/Baukasten
-		    cp -r Create-CSS $out/share/Album/Create-CSS
-		    cp -r Fotoalben $out/share/Album/Fotoalben
-		    cp -r Muster $out/share/Album/Muster
+          # Ressourcen ins Store-Verzeichnis kopieren
+	  mkdir -p $out/share/Album
+	  cp -r Baukasten $out/share/Album/Baukasten
+	  cp -r Create-CSS $out/share/Album/Create-CSS
+	  cp -r Fotoalben $out/share/Album/Fotoalben
+	  cp -r Muster $out/share/Album/Muster
 		  
-		    # Wrapper-Skript: beschreibbares Home-Verzeichnis verwenden
-		    mkdir -p $out/bin
-		    printf '#!/bin/sh\nDIR="$(cd "$(dirname "$0")" && pwd)"\nWORKDIR="$HOME/.local/share/Album"\nmkdir -p "$WORKDIR"\n\n# Ressourcen beim ersten Start ins Workdir kopieren\nif [ ! -d "$WORKDIR/Baukasten" ]; then\n  cp -r "$DIR/../share/Album/"* "$WORKDIR/"\n  chmod -R u+w "$WORKDIR"\nfi\n\ncd "$WORKDIR"\nexec "$DIR/Album" "$@"\n' \
-		      > $out/bin/album-launcher
-		    chmod +x $out/bin/album-launcher
+          # Wrapper-Skript: beschreibbares Home-Verzeichnis verwenden
+	  mkdir -p $out/bin
+	  printf '#!/bin/sh\nDIR="$(cd "$(dirname "$0")" && pwd)"\nWORKDIR="$HOME/.local/share/Album"\nmkdir -p "$WORKDIR"\n\n# Ressourcen beim ersten Start ins Workdir kopieren\nif [ ! -d "$WORKDIR/Baukasten" ]; then\n  cp -r "$DIR/../share/Album/"* "$WORKDIR/"\n  chmod -R u+w "$WORKDIR"\nfi\n\ncd "$WORKDIR"\nexec "$DIR/Album" "$@"\n' \
+> $out/bin/album-launcher
+	  chmod +x $out/bin/album-launcher
 		  
-		    # Desktop-Datei kopieren
-		    install -Dm644 ${desktopFile}/share/applications/album.desktop \
-		      $out/share/applications/album.desktop
+	  # Desktop-Datei kopieren
+	  install -Dm644 ${desktopFile}/share/applications/album.desktop \
+	  $out/share/applications/album.desktop
 		  
-		    # Exec auf das Wrapper-Skript setzen
-		    substituteInPlace $out/share/applications/album.desktop \
-		      --replace "Exec=Album" "Exec=$out/bin/album-launcher"
-		  '';
+	  # Exec auf das Wrapper-Skript setzen
+	  substituteInPlace $out/share/applications/album.desktop	\
+	  --replace "Exec=Album" "Exec=$out/bin/album-launcher"
+	  '';
 
           meta = with pkgs.lib; {
-            description = "GTK# .NET Application";
-            mainProgram = "Album";
-            license = licenses.gpl3Plus;
-            platforms = platforms.linux;
+	    description = "GTK# .NET Application";
+	    mainProgram = "Album";
+	    license = licenses.gpl3Plus;
+	    platforms = platforms.linux;
           };
         };
       in
       {
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
-            dotnet-sdk_9
-            gsettings-desktop-schemas
-            glib
-            gtk3
-            icu
-            pkg-config
-            nuget-to-json
-            wrapGAppsHook3
-          ];
+	devShells.default = pkgs.mkShell {
+	  packages = with pkgs; [
+	    dotnet-sdk_9
+	    gsettings-desktop-schemas
+	    glib
+	    gtk3
+	    gtk4
+	    icu
+	    pkg-config
+	    nuget-to-json
+	    libadwaita
+	    wrapGAppsHook3
+	  ];
 
-          shellHook = ''
-            export XDG_DATA_DIRS="${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:$XDG_DATA_DIRS"
+	  shellHook = ''
+	  export XDG_DATA_DIRS="${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:$XDG_DATA_DIRS"
+          export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath (with pkgs; [
+            gtk3
+            gtk4
+            glib
+            pango
+            cairo
+            gdk-pixbuf
+            libadwaita
+          ])}:$LD_LIBRARY_PATH
           '';
-        };
+	};
 
         packages.default = album;
 
-        apps.default = {
-          type = "app";
-          program = "${album}/bin/Album";
-        };
-      });
+	apps.default = {
+	  type = "app";
+	  program = "${album}/bin/Album";
+	};
+    });
 }

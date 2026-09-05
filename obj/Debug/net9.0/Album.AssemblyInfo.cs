e060a4e89d0a97bfe8a7dde4cfffd5a24fdb3c58
frieder <fribahnie@t-online.de>
2026-09-05 17:45:24 +0200

    neue Extension

--- obj/Debug/net9.0/Album.AssemblyInfo.cs
+++ obj/Debug/net9.0/Album.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Album")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02c68305ed8292f9977e348ccd3f8c909c75d5fa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c71031b4702b165b4c97d9f588b7e75c519a677f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Album")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Album")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

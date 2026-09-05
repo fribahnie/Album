5a75943f3e95478836f1b209f72c985f8b3448a8
frieder <fribahnie@t-online.de>
2026-09-05 17:47:08 +0200

    jetzt läuft es durch!

--- obj/Debug/net9.0/Album.AssemblyInfo.cs
+++ obj/Debug/net9.0/Album.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Album")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e060a4e89d0a97bfe8a7dde4cfffd5a24fdb3c58")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a63e03e17cc22efa6606bf74e405b7426f565f5e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Album")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Album")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

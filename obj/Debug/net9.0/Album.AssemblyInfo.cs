f67ecdb7c121924b9d98d4fd19b4ff23568f0cac
frieder <fribahnie@t-online.de>
2026-09-07 19:56:53 +0200

    new vscode

--- obj/Debug/net9.0/Album.AssemblyInfo.cs
+++ obj/Debug/net9.0/Album.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Album")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edc731d4a118997d541ccf5067df46758f52a7fa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd31dd1f4826534e6a934984bdf1cd47dbadc6ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("Album")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Album")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

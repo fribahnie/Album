b4b9c847d41a1b6cc14b4d8d2d61239a4de8a3b6
frieder <fribahnie@t-online.de>
2026-09-07 20:05:44 +0200

    alte Dateien gelöscht

--- obj/Debug/net9.0/Album.AssemblyInfo.cs
+++ obj/Debug/net9.0/Album.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Album")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f67ecdb7c121924b9d98d4fd19b4ff23568f0cac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a968c70cc42143de01f7ca089199ddeeadeeea60")]
 [assembly: System.Reflection.AssemblyProductAttribute("Album")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Album")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

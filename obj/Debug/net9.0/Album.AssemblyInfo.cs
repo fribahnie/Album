b643cafdb5ad8e9d6a6e7543c3a3af2ba46c84bf
frieder <fribahnie@t-online.de>
2026-09-07 18:39:54 +0200

    update

--- obj/Debug/net9.0/Album.AssemblyInfo.cs
+++ obj/Debug/net9.0/Album.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Album")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a75943f3e95478836f1b209f72c985f8b3448a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f72736769544ccf1cda59c90e3ddfe5dcc7f3540")]
 [assembly: System.Reflection.AssemblyProductAttribute("Album")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Album")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

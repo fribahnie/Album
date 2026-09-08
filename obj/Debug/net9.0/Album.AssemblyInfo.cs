de6e67cc5d3af85227085b743d018ec9ba77237b
frieder <fribahnie@t-online.de>
2026-09-08 11:19:17 +0200

    Neuer Desktop-Eintrag

--- obj/Debug/net9.0/Album.AssemblyInfo.cs
+++ obj/Debug/net9.0/Album.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Album")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4b9c847d41a1b6cc14b4d8d2d61239a4de8a3b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea429cbff61aaead81ea3d1b098b50e165d4d507")]
 [assembly: System.Reflection.AssemblyProductAttribute("Album")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Album")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

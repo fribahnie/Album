56c1579181372c21788ae3bbaabce7c4c3973a6b
frieder <fribahnie@t-online.de>
2026-09-08 11:27:46 +0200

    alles synchronisieren

--- obj/Debug/net9.0/Album.AssemblyInfo.cs
+++ obj/Debug/net9.0/Album.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Album")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de6e67cc5d3af85227085b743d018ec9ba77237b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90fdc2dd5a17e57472f74009a4c9d1be388f735b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Album")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Album")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

c92463f034d65e1d8e61868866c9ccb4a7035ca7
frieder <fribahnie@t-online.de>
2026-09-10 18:41:11 +0200

    Neue Release erstellt

--- obj/Debug/net9.0/Album.AssemblyInfo.cs
+++ obj/Debug/net9.0/Album.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Album")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0d1e3fb4e0996ba2872dd304cd816dbf20edcb6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+caee5410e2f1ddcaf87c4d8f092a7bfc844e70f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Album")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Album")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
